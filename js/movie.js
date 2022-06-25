82671d562f743730dff0ec850f462734de2cb46c
ahmed <a-5amd@hotmail.com>
2022-06-25 23:40:41 +0300

    update5

--- js/movie.js
+++ js/movie.js
@@ -1,19 +1,18 @@
 Storage_Info =() =>  {
 
-  let email = document.getElementById("btn_sign").value;
+  let email = document.getElementById("email").value;
+ 
 
   localStorage.setItem("email", email);
 
   let password = document.getElementById("password").value;
+  
 
   localStorage.setItem("password", password);
 
-  localStorage.getItem('email',"password");
+  localStorage.getItem("email","password");
 
 }
-
-
-
 
 
 
